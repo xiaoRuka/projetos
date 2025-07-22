04c677b54006a638cedc90d3a15fa2c1aa66b5f7
xiaoRuka <rukamatsu@gmail.com>
2025-07-22 12:38:26 -0300

    feat: quizSlice
    creating a quiz slice with redux

---
+++ quizcopy/src/store/quizSlice.js
@@ -0,0 +1,35 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  questions: [],
+  currentQuestionIndex: 0,
+  answers: [],
+  isQuizCompleted: false,
+  score: 0,
+  timeLeft: 300,
+  isTimerActive: false,
+  showExplanation: false,
+};
+
+const quizSlice = createSlice({
+  name: "Quiz",
+  initialState,
+  reducers: {
+    setQuestions: (state, action) => {
+      state.questions = action.payload;
+    },
+
+    startQuiz: (state) => {
+      state.currentQuestionIndex = 0;
+      state.answers = [];
+      state.isQuizCompleted = false;
+      state.score = 0;
+      state.timeLeft = 300;
+      (state.isTimerActive = true), (state.showExplanation = false);
+    },
+  },
+});
+
+export const { setQuestions, startQuiz } = quizSlice.actions;
+
+export default quizSlice.reducer;
