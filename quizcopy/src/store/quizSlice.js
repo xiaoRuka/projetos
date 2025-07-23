89eb31f4a947659f0d4a7c0d769a032b54f55793
xiaoRuka <rukamatsu@gmail.com>
2025-07-23 12:22:36 -0300

    feat: timer and questions
    creating a timer to use in questions and states for answers

--- quizcopy/src/store/quizSlice.js
+++ quizcopy/src/store/quizSlice.js
@@ -27,9 +27,34 @@ const quizSlice = createSlice({
       state.timeLeft = 300;
       (state.isTimerActive = true), (state.showExplanation = false);
     },
+
+    answerQuestions: (state, action) => {
+      const currectQuestion = state.questions[state.currentQuestionIndex];
+      const isCorrect = action.payload.selectedOption === currectQuestion.correctAnswer;
+
+      const answer = {
+        questionId: currectQuestion.id, 
+        selectedOption: action.payload.selectedOption,
+        isCorrect
+      };
+
+      state.answers.push[answer];
+
+      if(isCorrect){
+        state.score += 1;
+      }
+    }
+
+    decreamentTimer: (state) => {
+      if (state.timeLeft > 0 && state.isTimerActive) {
+        state.timeLeft -= 1;
+      } else if (state.timeLeft === 0) {
+        (state.isQuizCompleted = true), (state.isTimerActive = false);
+      }
+    },
   },
 });
 
-export const { setQuestions, startQuiz } = quizSlice.actions;
+export const { setQuestions, startQuiz, decreamentTimer, answerQuestions} = quizSlice.actions;
 
 export default quizSlice.reducer;
