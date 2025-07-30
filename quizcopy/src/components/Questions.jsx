05da63c62cc575cd0e7eddd0f2ee0ff6f96e584e
xiaoRuka <rukamatsu@gmail.com>
2025-07-30 11:48:33 -0300

    feat: creating new functions
    created function to answer a event, and to buttons Next and previous

--- quizcopy/src/components/Questions.jsx
+++ quizcopy/src/components/Questions.jsx
@@ -1,15 +1,38 @@
 import React from "react";
-import { ArrowLeft, ArrowRight, CheckCircle } from "lucide-react";
-import { useSelector } from "react-redux";
+import { ArrowLeft, ArrowRight, CheckCircle, XCircle } from "lucide-react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  answerQuestions,
+  nextQuestion,
+  previousQuestion,
+} from "../store/quizSlice";
 
 function Questions() {
+  const dispatch = useDispatch();
   const { questions, currentQuestionIndex, answers, showExplanation } =
     useSelector((state) => state.quiz);
 
   const currentQuestion = questions[currentQuestionIndex];
   const currentAnswer = answers.find(
-    (answer) => answer.questionId == currentQuestion.id
+    (answer) => answer.questionId === currentQuestion.id
   );
+
+  // Answer event
+
+  const handleOptionClick = (optionindex) => {
+    if (!currentAnswer) {
+      dispatch(answerQuestions({ selectedOption: optionindex }));
+    }
+  };
+
+  // handleNext and previous
+  const handleNext = () => {
+    dispatch(nextQuestion());
+  };
+
+  const handlePrevious = () => {
+    dispatch(previousQuestion());
+  };
 
   return (
     <div className="max-w-4xl mx-auto">
@@ -18,14 +41,15 @@ function Questions() {
           <h2 className="text-2xl font-bold text-gray-800 mb-6 leading-realxed">
             {currentQuestion.question}
           </h2>
+
           {/*Display Dynamic Answer*/}
           <div className="grid gap-4">
             {currentQuestion.options.map((option, index) => {
               const isSelected = currentAnswer?.selectedOption === index;
-              const isCorrect = index == currentQuestion.correctAnswer;
+              const isCorrect = index === currentQuestion.correctAnswer;
               const isWrong = isSelected && !isCorrect && showExplanation;
 
-              let buttonClass = `w-full p4 text-left rounde-xl border-1 border-gray-300 transition-all duration-200`;
+              let buttonClass = `w-full p4 text-left rounded-xl border-1 border-gray-300 transition-all duration-200`;
 
               if (showExplanation) {
                 if (isCorrect) {
@@ -38,13 +62,23 @@ function Questions() {
               } else if (isSelected) {
                 buttonClass += `border-blue-200 bg-blue-50 text-blue-800`;
               } else {
-                buttonClass += `border-gray-200 bg-white text-gray-700 hover: border-blue-300 hover:shadow-md`;
+                buttonClass += `border-gray-200 bg-white text-gray-700 hover:border-blue-300 hover:shadow-md`;
               }
 
               return (
-                <button key={index}>
+                <button
+                  key={index}
+                  className={buttonClass}
+                  onClick={() => handleOptionClick(index)}
+                >
                   <div className="flex items-center justify-between">
-                    <span>{option}</span>
+                    <span className="text-lg">{option}</span>
+                    {showExplanation && isCorrect && (
+                      <CheckCircle size={24} className="text-green-600" />
+                    )}
+                    {showExplanation && isWrong && (
+                      <XCircle size={24} className="text-red-600" />
+                    )}
                   </div>
                 </button>
               );
@@ -53,27 +87,44 @@ function Questions() {
         </div>
 
         {/*Show explanation */}
-        <div className="bg-blue-50 border-l-4 border-blue-400 p-4 mb-6 rounded-r-lg">
-          <div className="flex">
-            <div className="flex-shrink-0">
-              <CheckCircle className="h-5 w-5 text-blue" />
-            </div>
-            <div className="ml-3">
-              <p className="text-blue-800 font-medium">Explicação:</p>
-              <p className="text-blue-700 mt-1">Explicação da questão atual</p>
+        {showExplanation && currentQuestion.explanation && (
+          <div className="bg-blue-50 border-l-4 border-blue-400 p-4 mb-6 rounded-r-lg">
+            <div className="flex">
+              <div className="flex-shrink-0">
+                <CheckCircle className="h-5 w-5 text-blue" />
+              </div>
+              <div className="ml-3">
+                <p className="text-blue-800 font-medium">Explicação:</p>
+                <p className="text-blue-700 mt-1">
+                  {currentQuestion.explanation}
+                </p>
+              </div>
             </div>
           </div>
-        </div>
+        )}
+
         {/*Display the Buttons*/}
         <div className="flex justify-between items-center">
-          <button className="flex items-center space-x-2 px-6 py-3 bg-gray-100 text-gray-700 rounded-lg hover:bg-gray-200 disabled:opacity-5 disabled:cursor-not-allowed transition-all duration-200">
+          <button
+            className="flex items-center space-x-2 px-6 py-3 bg-gray-100 text-gray-700 rounded-lg hover:bg-gray-200 disabled:opacity-5 disabled:cursor-not-allowed transition-all duration-200"
+            onClick={handlePrevious}
+          >
             <ArrowLeft size={20} />
             <span>Anterior</span>
           </button>
-          <button className="flex items-center space-x-2 px-6 py-3 bg-gray-100 text-gray-700 rounded-lg hover:bg-gray-200 disabled:opacity-5 disabled:cursor-not-allowed transition-all duration-200">
-            <ArrowRight size={20} />
-            <span>Próxima</span>
-          </button>
+          {showExplanation && (
+            <button
+              className="flex items-center space-x-2 px-6 py-3 bg-gray-100 text-gray-700 rounded-lg hover:bg-gray-200 disabled:opacity-5 disabled:cursor-not-allowed transition-all duration-200"
+              onClick={handleNext}
+            >
+              <ArrowRight size={20} />
+              <span>
+                {currentQuestionIndex === questions.length - 1
+                  ? "Terminar o Quiz"
+                  : "Próxima Questão"}
+              </span>
+            </button>
+          )}
         </div>
       </div>
     </div>
