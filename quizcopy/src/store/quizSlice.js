b0ebcf767dc9a2e1bd3c859360a56f3df4ce77bd
xiaoRuka <rukamatsu@gmail.com>
2025-07-30 11:43:55 -0300

    feat: next and previous question, and reset quiz
    Add states for NextQuestion, Previous Question and to reset the quiz

--- quizcopy/src/store/quizSlice.js
+++ quizcopy/src/store/quizSlice.js
@@ -30,20 +30,50 @@ const quizSlice = createSlice({
 
     answerQuestions: (state, action) => {
       const currectQuestion = state.questions[state.currentQuestionIndex];
-      const isCorrect = action.payload.selectedOption === currectQuestion.correctAnswer;
+      const isCorrect =
+        action.payload.selectedOption === currectQuestion.correctAnswer;
 
       const answer = {
-        questionId: currectQuestion.id, 
+        questionId: currectQuestion.id,
         selectedOption: action.payload.selectedOption,
-        isCorrect
+        isCorrect,
       };
 
-      state.answers.push[answer];
+      state.answers.push(answer);
 
-      if(isCorrect){
+      if (isCorrect) {
         state.score += 1;
       }
-    }
+      state.showExplanation = true;
+    },
+
+    nextQuestion: (state) => {
+      state.showExplanation = false;
+      if (state.currentQuestionIndex < state.questions.length - 1) {
+        state.currentQuestionIndex += 1;
+      } else {
+        state.isQuizCompleted = true;
+        state.isTimerActive = false;
+      }
+    },
+
+    previousQuestion: (state) => {
+      if (state.currentQuestionIndex > 0) {
+        state.currentQuestionIndex -= 1;
+
+        // Remove the answer for the current questios is going back
+        state.answers = state.answers.filter(
+          (answer) =>
+            answer.questionId !== state.questions[state.currentQuestionIndex].id
+        );
+
+        state.showExplanation = false;
+
+        // recalculate
+
+        state.score = state.answers.filter((answer) => answer.isCorrect).length;
+      }
+    },
 
     decreamentTimer: (state) => {
       if (state.timeLeft > 0 && state.isTimerActive) {
@@ -52,9 +82,27 @@ const quizSlice = createSlice({
         (state.isQuizCompleted = true), (state.isTimerActive = false);
       }
     },
+
+    resetQuiz: (state) => {
+      state.currentQuestionIndex = 0;
+      state.answers = [];
+      state.isQuizCompleted = false;
+      state.score + 0;
+      state.timeLeft = 300;
+      state.isTimerActive = false;
+      state.showExplanation = false;
+    },
   },
 });
 
-export const { setQuestions, startQuiz, decreamentTimer, answerQuestions} = quizSlice.actions;
+export const {
+  setQuestions,
+  startQuiz,
+  decreamentTimer,
+  answerQuestions,
+  nextQuestion,
+  previousQuestion,
+  resetQuiz,
+} = quizSlice.actions;
 
 export default quizSlice.reducer;
